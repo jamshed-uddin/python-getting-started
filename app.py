f894dd05d1684e17a61b515272027b6ca8e291a7
Md Jamshed Uddin <jamsheduddin03@gmail.com>
2025-05-18 00:34:45 +0600

    getting started with variables and strings

---
+++ app.py
@@ -0,0 +1,27 @@
+print("hello")
+
+
+#  primitive data types
+
+count = 1
+
+name = 'John Doe'
+
+is_active = True
+
+
+# string
+
+str = 'hello'
+
+# get the length
+print(len(str))
+
+# get certain character
+
+print(str[1])
+print(str[-1])
+print(str[0:3])
+print(str[0:])
+print(str[:3])
+print(str[:])
